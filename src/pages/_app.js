bafe9ac18aa03ad90c82f66020af0c8e01feeab0
Nicolás Leal <lealnicolas96@gmail.com>
2022-09-25 23:35:25 -0400

    Handle loading state when change screen

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,25 @@
 import { useEffect } from 'react';
+import Router from 'next/router';
 import Head from 'next/head';
 import { CookiesProvider } from 'react-cookie';
+import NProgress from 'nprogress';
 import dayjs from 'dayjs';
 import 'dayjs/locale/es';
 import UserContextProvider from '@contexts/UserContext/UserContext';
 import '../styles/globals.css';
+import '../styles/nprogress.css';
 
 const metaTitle = 'Reversed Logistic App';
 const metaDescription =
   'Una aplicación para controlar la logística inversa de tu empresa cervecera';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+NProgress.configure({
+  showSpinner: false,
+});
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
